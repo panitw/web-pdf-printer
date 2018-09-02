d66b86a2a5cd00521f60a097a1ca35994ebb0017
Panit Wechasil <panitw@hotmail.com>
2018-09-02 13:17:03 +0700

    Add time

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const puppeteer = require('puppeteer');
 const temp = require('temp');
 const fs = require('fs');
+const moment = require('moment');
 
 app.get('/node/pdf-printer/download', (req, res) => {
 	let url = req.query.url;
@@ -34,16 +35,19 @@ app.get('/node/pdf-printer/download', (req, res) => {
 		.then(() => {
 			var ft = [];
 			ft.push('<div style="font-size:10px; width:100%; text-align:right; padding-right: 20px; padding-left: 20px;">');
-			ft.push('<div style="display: inline-block; float: left;">');
+			ft.push('<table style="width: 100%"><tr><td style="text-align: left; width: 33%;">');
 			ft.push('Printed By: ');
 			ft.push(printedBy);
-			ft.push('</div>');
-			ft.push('<div style="display: inline-block; float: right;">');
+			ft.push('</td>');
+			ft.push('<td style="text-align: center;">');
+			ft.push(moment().format('DD/MM/YYYY HH:mm:ss'));
+			ft.push('</td>');
+			ft.push('<td style="text-align: right; width: 33%;">');
 			ft.push('Page <span class="pageNumber"></span> ');
 			ft.push('of ');
 			ft.push('<span class="totalPages"></span>');
-			ft.push('</div>');
-			ft.push('</div>');
+			ft.push('</td></tr>');
+			ft.push('</table>');
 
 			tempName = temp.path({suffix: '.pdf'});
 			currentPage.emulateMedia('print');
