4ae77bad6c51897e7f031b71d8d590f9e5a68acf
Panit Wechasil <panitw@hotmail.com>
2018-09-02 08:05:24 +0700

    Add header support

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 
 app.get('/node/pdf-printer/download', (req, res) => {
 	let url = req.query.url;
+	let printedBy = req.query.by || '-';
 	let currentBrowser = null;
 	let currentPage = null;
 	let tempName = null;
@@ -28,11 +29,33 @@ app.get('/node/pdf-printer/download', (req, res) => {
 		})
 		.then((page) => {
 			currentPage = page;
-			return currentPage.goto(url, {waitUntil: 'networkidle2'});
+			return currentPage.goto(url, {waitUntil: 'networkidle0'});
 		})
 		.then(() => {
+			var ft = [];
+			ft.push('<div style="font-size:10px; width:100%; text-align:right; padding-right: 20px; padding-left: 20px;">');
+			ft.push('<div style="display: inline-block; float: left;">');
+			ft.push('Printed By: ');
+			ft.push(printedBy);
+			ft.push('</div>');
+			ft.push('<div style="display: inline-block; float: right;">');
+			ft.push('Page <span class="pageNumber"></span> ');
+			ft.push('of ');
+			ft.push('<span class="totalPages"></span>');
+			ft.push('</div>');
+			ft.push('</div>');
+
 			tempName = temp.path({suffix: '.pdf'});
-			return currentPage.pdf({path: tempName, format: format});
+			currentPage.emulateMedia('print');
+			return currentPage.pdf({
+				path: tempName,
+				format: format,
+				displayHeaderFooter: true,
+				footerTemplate: ft.join(''),
+				margin: {
+					bottom: "100px"
+				}
+			});
 		})
 		.then(() => {
 			return currentBrowser.close();
